f917473ff3e2214fda40144a53e614710ac7f33e
MohammadF <faraj4497@gmail.com>
2024-01-21 21:30:19 +0200

    QR - hotfix

--- app/src/main/java/com/bzu/auth/activity/QrCode.java
+++ app/src/main/java/com/bzu/auth/activity/QrCode.java
@@ -1,11 +1,18 @@
 package com.bzu.auth.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.bzu.auth.R;
+import com.google.zxing.Result;
 
 public class QrCode extends AppCompatActivity {
 
@@ -13,5 +20,25 @@ public class QrCode extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+        CodeScanner mCodeScanner;
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(QrCode.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
     }
 }
